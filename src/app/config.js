f7a6c125106f8056d108023e0654453301a57fd8
steveoh <sgourley@utah.gov>
2014-07-24 20:12:04 -0600

    Release v0.1.1

--- src/app/config.js
+++ src/app/config.js
@@ -19,7 +19,7 @@ define(['dojo/has'], function (has) {
 
         // version.: String
         //      The version number.
-        version: '0.1.0',
+        version: '0.1.1',
 
         // apiKey: String
         //      The api key used for services on api.mapserv.utah.gov
