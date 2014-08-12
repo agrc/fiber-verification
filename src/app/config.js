458bce1ade2ab2b1dbf73c385dbbe0a101820e6b
Scott Davis <stdavis@utah.gov>
2014-08-12 07:29:36 -0600

    Release v0.2.0

--- src/app/config.js
+++ src/app/config.js
@@ -40,7 +40,7 @@ define(['dojo/has'], function (has) {
 
         // version.: String
         //      The version number.
-        version: '0.1.3',
+        version: '0.2.0',
 
         // apiKey: String
         //      The api key used for services on api.mapserv.utah.gov
